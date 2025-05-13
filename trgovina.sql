05b622d8a3f5b432584088ced2605c3209323828
DavidMatijevic <208679348+DavidMatijevic@users.noreply.github.com>
2025-05-13 19:28:12 +0200

    Update trgovina.sql
    Vjezba ERA

--- trgovina.sql
+++ trgovina.sql
@@ -2,20 +2,42 @@
 
 --use trgovina;
 
---create table racuni(
+--create table (
 --sifra int,
 --broj varchar(10),
 --kupac varchar(100)
 --);
 
---create table proizvodi(
+--create table (
 --sifra int,
 --naziv varchar(50),
 --cijena decimal(18,2)
 --);
 
-create table stavke(
-racun int,
-proizvod int,
-kolicina decimal(5,3)
+
+use master;
+go
+drop database if exists trgovina;
+go
+create database trgovina;
+go
+use trgovina;
+
+create table racuni(
+sifra int not null primary key identity(1,1),
+broj varchar(50) not null,
+kupac varchar(100),
 );
+
+create table proizvodi(
+sifra int not null primary key identity(1,1),
+naziv varchar(50),
+cijena varchar(100)
+);
+
+create table stavke(
+sifra int not null references racuni(sifra),
+proizvod int not null references proizvodi(sifra),
+cijena decimal(5,3) not null,
+);
+
