73ad4b1a31c329030e72d0e7ad67332af32b05ee
DavidMatijevic <208679348+DavidMatijevic@users.noreply.github.com>
2025-05-27 19:27:27 +0200

    Update i delete naredbe

--- Update_delete.sql
+++ Update_delete.sql
@@ -1,1 +1,36 @@
-﻿
+﻿use edunovawp8;
+
+--Update naredba
+
+select * from smjerovi where sifra=2;
+
+
+update smjerovi 
+set aktivan=1
+where sifra=2;
+
+
+
+update smjerovi set
+cijena=1000,
+datumpokretanja='2024-02-29'
+where sifra=3;
+
+
+update smjerovi set cijena=1000 where cijena is null;
+
+
+-- dižemo cijenu svih svmjerova za 12%
+
+update smjerovi set cijena = cijena * 0.9;
+
+
+-- Delete naredba
+
+
+
+select * from smjerovi
+
+delete smjerovi where sifra=1;
+delete grupe where smjer=1;
+delete clanovi where grupa in (select sifra from grupe where smjer=1)
