0028900fff19a3483d74ac832f6210b3c3b6d776
DavidMatijevic <208679348+DavidMatijevic@users.noreply.github.com>
2025-06-06 22:47:08 +0200

    Create Završni rad.sql

---
+++ Završni rad.sql
@@ -0,0 +1,67 @@
+﻿USE MASTER
+GO
+DROP DATABASE IF EXISTS VideoIgre
+GO
+CREATE DATABASE VideoIgre
+GO
+
+USE VideoIgre
+
+-- Unos entiteta
+CREATE TABLE Igra (
+    Sifra INT PRIMARY KEY IDENTITY(1,1),
+    Naziv NVARCHAR(100) NOT NULL,
+    Godinaizdanja INT,
+    Zanr VARCHAR(50),
+    Opis VARCHAR(500)
+);
+
+CREATE TABLE Platforma (
+    Sifra INT PRIMARY KEY IDENTITY(1,1),
+	Sifra_igre INT not null FOREIGN KEY REFERENCES Igra(sifra),
+    Naziv VARCHAR(50),
+    Proizvodjac VARCHAR(50)
+);
+
+CREATE TABLE Razvijac (
+    Sifra INT PRIMARY KEY IDENTITY(1,1),
+	Sifra_igre INT not null FOREIGN KEY REFERENCES Igra(sifra),
+    Naziv VARCHAR(100),
+    Sjedište VARCHAR(100)
+);
+
+CREATE TABLE Izdavac (
+    Sifra INT PRIMARY KEY IDENTITY(1,1),
+	Sifra_igre INT not null FOREIGN KEY REFERENCES Igra(sifra),
+    Naziv VARCHAR(100),
+    Sjedište VARCHAR(100)
+);
+
+CREATE TABLE Igrac (
+    Sifra INT PRIMARY KEY IDENTITY(1,1),
+    Korisnicko_ime NVARCHAR(50) UNIQUE NOT NULL,
+    Email NVARCHAR(100) UNIQUE NOT NULL,
+    Datum_registracije DATE DEFAULT GETDATE()
+);
+
+
+-- Unos igre
+INSERT INTO Igra (Naziv,Godinaizdanja,Zanr,Opis)
+VALUES
+('Cyberpunk 2077', 2020, 'RPG', 'Futuristički RPG iz dystopijskog svijeta')
+;
+
+-- Unos platforme
+INSERT INTO Platforma (Sifra_igre,Naziv,Proizvodjac)
+VALUES
+('1','PlayStation 5', 'Sony'),
+('2','Xbox Series X','Microsoft'),
+('3','PC','Razni');
+
+INSERT INTO Razvijac (Sifra_igre,Naziv, Sjedište) 
+VALUES 
+('1','CD Projekt Red', 'Poljska')
+
+INSERT INTO  Izdavac (Sifra_igre,Naziv,Sjedište)
+VALUES 
+('1','CD Projekt Red', 'Poljska')
