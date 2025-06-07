1709aae28947f31f15a5d4aa49e0e4ef2a917eec
DavidMatijevic <matijevicdavid4@gmail.com>
2025-06-07 09:35:17 +0200

    Update Završni rad.sql

--- Zavrsni rad/Završni rad.sql
+++ Zavrsni rad/Završni rad.sql
@@ -8,7 +8,7 @@ GO
 USE VideoIgreM
 
 -- Unos entiteta
-CREATE TABLE Igre (
+CREATE TABLE Igra (
     Sifra INT PRIMARY KEY IDENTITY(1,1),
     Naziv NVARCHAR(100) NOT NULL,
     Godinaizdanja INT,
@@ -16,28 +16,28 @@ CREATE TABLE Igre (
     Opis VARCHAR(500)
 );
 
-CREATE TABLE Platforme (
+CREATE TABLE Platforma (
     Sifra INT PRIMARY KEY IDENTITY(1,1),
 	Sifra_igre INT not null FOREIGN KEY REFERENCES Igra(sifra),
     Naziv VARCHAR(50),
     Proizvodjac VARCHAR(50)
 );
 
-CREATE TABLE Razvijaci (
+CREATE TABLE Razvijac (
     Sifra INT PRIMARY KEY IDENTITY(1,1),
 	Sifra_igre INT not null FOREIGN KEY REFERENCES Igra(sifra),
     Naziv VARCHAR(100),
     Sjedište VARCHAR(100)
 );
 
-CREATE TABLE Izdavaci (
+CREATE TABLE Izdavac (
     Sifra INT PRIMARY KEY IDENTITY(1,1),
 	Sifra_igre INT not null FOREIGN KEY REFERENCES Igra(sifra),
     Naziv VARCHAR(100),
     Sjedište VARCHAR(100)
 );
 
-CREATE TABLE Igraci (
+CREATE TABLE Igrac (
     Sifra INT PRIMARY KEY IDENTITY(1,1),
     Korisnicko_ime NVARCHAR(50) UNIQUE NOT NULL,
     Email NVARCHAR(100) UNIQUE NOT NULL,
@@ -46,23 +46,24 @@ CREATE TABLE Igraci (
 
 
 -- Unos igre
-INSERT INTO Igre (Naziv,Godinaizdanja,Zanr,Opis)
+INSERT INTO Igra (Naziv,Godinaizdanja,Zanr,Opis)
 VALUES
 ('Cyberpunk 2077', 2020, 'RPG', 'Futuristički RPG iz dystopijskog svijeta')
 ;
 
 -- Unos platforme
-INSERT INTO Platforme (Sifra_igre,Naziv,Proizvodjac)
+INSERT INTO Platforma (Sifra_igre,Naziv,Proizvodjac)
 VALUES
 (1,'PlayStation 5', 'Sony'),
 (1,'Xbox Series X','Microsoft'),
 (1,'PC','Razni');
 -- Unos razvijaca
-INSERT INTO Razvijaci (Sifra_igre,Naziv, Sjedište) 
+INSERT INTO Razvijac (Sifra_igre,Naziv, Sjedište) 
 VALUES 
 (1,'CD Projekt Red', 'Poljska')
 -- Unos izdavaca
-INSERT INTO  Izdavaci (Sifra_igre,Naziv,Sjedište)
+INSERT INTO  Izdavac (Sifra_igre,Naziv,Sjedište)
 VALUES 
 (1,'CD Projekt Red', 'Poljska')
 
+select * from Izdavac
